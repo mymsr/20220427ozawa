f5d78a1df01abe4075302e11a9837eaca84b2e77
tarako <m.chi.0804@gmail.com>
2022-05-06 13:18:21 +0900

    Responsive

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-
+const target = document.getElementById("responsiveMenu");
+target.addEventListener ('click',() => {
+  target.classList.toggle('open');
+  const nav = document.getElementById('responsiveNav');
+  nav.classList.toggle('in');
+});
